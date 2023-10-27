9bca93294384b2cd86d3607cf71603ac3d1fcac2
sidesg <sidesg@gmail.com>
2023-10-27 17:01:16 -0400

    Uploaded files now moved out of watch folder

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 import yaml
 import logging
+import shutil
 
 from ckanapi import RemoteCKAN
 from pathlib import Path
@@ -13,45 +14,53 @@ def main():
     logfolder = Path("logs")
     if not logfolder.exists():
         logfolder.mkdir()
-    logging.basicConfig(filename=f'logs/pipeline-{timestamp}.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(filename=(logfolder / f'pipeline-{timestamp}.log'), encoding='utf-8', level=logging.DEBUG)
 
+    #Load dict of resource-file key-value pairs
+    with open('updates.yml', 'r') as file:
+        updates: dict = yaml.safe_load(file)
+
+    #Load API key
     keypath = ".creds/apikey.txt"
     if not Path(keypath).exists():
         createkey()
     ua = 'cmtq/1.0 (+https://www.cinematheque.qc.ca/)'
 
-    with open('updates.yml', 'r') as file:
-        updates: dict = yaml.safe_load(file)
-
     with open(keypath, "r") as infile:
-        apikey = infile.read()
+        apikey: str = infile.read()
 
+    #Upload files
     with RemoteCKAN('https://www.donneesquebec.ca', 
                     user_agent=ua, apikey=apikey) as conn:
         for ress_id, path in updates.items():
+            fullpath: str = "data/" + path
             try:
                 resource = CKANResource.from_id(ress_id, conn)
             except Exception:
-                logging.error(f"\"{ress_id}\". identifiant de ressource introuvable")
+                logging.error(f"\"{ress_id}\". Identifiant de ressource introuvable sur Données Québec")
                 continue
 
-            if not Path("data/" + path).exists():
-                logging.error(f"\"{ress_id}\" : \"{path}\" chemin invalide")
+            if not Path(fullpath).exists():
+                logging.error(f"\"{ress_id}\" : \"{path}\" chemin de fichier invalide")
                 continue
 
             try:
-                resource.update_file("data/" + path, conn)
+                resource.update_file(fullpath, conn)
             except Exception:
                 logging.error(f"\"{ress_id}\" : \"{path}\" échec de la mise à jour")
                 continue
 
-def createkey():
+            shutil.move(fullpath, "uploaded")
+            logging.info(f"{path} déplacé au dossier 'uploaded'")
+
+def createkey() -> None:
     keyfolder = Path(".creds")
     keyfolder.mkdir()
     apikey = input("Saisir votre cle API : ").strip()
 
     with open(keyfolder / "apikey.txt", "w") as f:
         f.write(apikey)
+
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -4,6 +4,6 @@ Simple tool to update file in a list of CKAN resources on Données Québec.
 
 Needs the following files/folders:
 
-- `./updates.yml`: List of key: value pairs where each key is the resource's ID and the value is the name of the file in `data` folder.
+- `./updates.yml`: List of key: value pairs where each key is the resource's ID and the value is the name of the file in `data` folder, e.g., `1234-abcd: datafile.csv`.
 - `./data`: data files to be uploaded, documented in `updates.yml`.
 - `./.creds/apikey.txt`: plain text file containing the Données Québec API key.
